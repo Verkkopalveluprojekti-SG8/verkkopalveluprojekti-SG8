483475cd7059b0397d9fe0eace417ca86125f78c
Jorma <joel.oksanen1223@gmail.com>
2023-03-15 12:40:28 +0200

    alku

--- verkkopalveluprojekti/src/App.js
+++ verkkopalveluprojekti/src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <div  class="header">
+        <h1>RYYST</h1>
+      </div>
+
+
+
+   <h2>Asiakaspalvelu</h2>
+
+
+   <div class="categories">
+
+      <a class="1">Alkoholittomat</a>
+
+      <a class="2">Viinit</a>
+
+      <a class="3">Miedot juomat</a>
+
+      <a class="4">Vodkat ja viinat</a>
+
+      <a class="5">Liköörit</a>
+
+      <a class="6">Muut</a>
+
+
+   </div>
+   </div>
   );
 }
 
