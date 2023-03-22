99711dbec1a04e74bc164f0eba8ab8f3917cf6de
Villedille <n2ravi00.students.oamk.fi>
2023-03-22 15:06:29 +0200

    navbari

--- verkkopalveluprojekti/src/App.js
+++ verkkopalveluprojekti/src/App.js
@@ -1,5 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/footer';
+import Header from './components/header';
+import Navbar from './components/navbar';
+import Products from './pages/products';
+import Home from './pages/home';
+
+
 
 
 function App() {
