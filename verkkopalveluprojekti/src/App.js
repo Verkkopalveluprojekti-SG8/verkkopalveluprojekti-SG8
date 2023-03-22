1734c5ccd8d7eb8862327818d1fe6d2ced2adb04
Villedille <n2ravi00.students.oamk.fi>
2023-03-22 15:56:49 +0200

    navbar, home, footer, index ja products js tekoa

--- verkkopalveluprojekti/src/App.js
+++ verkkopalveluprojekti/src/App.js
@@ -5,45 +5,21 @@ import Header from './components/header';
 import Navbar from './components/navbar';
 import Products from './pages/products';
 import Home from './pages/home';
+import {Routes, Route} from 'react-router-dom';
 
 
 
 
 function App() {
   return (
-    <div>
-      <div  class="logo">
-        <h1>RYYST</h1>
-      </div>
-
-
-    <div class="header">
-
-
-    <input class="searchbar" type="text" placeholder="Search.."></input>
-
-    <h4>"Ryyst and Jyyst"</h4>
-      </div>
-      
-
-
-   <div class="categories">
-
-      <a class="c" href="https://www.oispakaljaa.com/">Alkoholittomat</a>
-
-      <a class="c" href="https://www.oispakaljaa.com/">Viinit</a>
-
-      <a class="c" href="https://www.oispakaljaa.com/">Miedot juomat</a>
-
-      <a class="c" href="https://www.oispakaljaa.com/">Vodkat ja viinat</a>
-
-      <a class="c" href="https://www.oispakaljaa.com/">Liköörit</a>
-
-      <a class="c" href="https://www.oispakaljaa.com/">Muut</a>
-
-
-   </div>
-   </div>
+    <>
+    <div className='container'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        </Routes>
+        </div>
+   <Footer />
+   </>
   );
 }
 
