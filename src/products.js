6fd99fea0aeb28238e6c06b82055a00f29ef15b6
Jorma <joel.oksanen1223@gmail.com>
2023-04-27 17:05:47 +0300

    Merge branch 'main' of https://github.com/Verkkopalveluprojekti-SG8/verkkopalveluprojekti-SG8

---
+++ src/products.js
@@ -0,0 +1,60 @@
+import gambina from "./assets/gambina.png";
+import product2 from "./assets/karhu.png";
+import product3 from "./assets/lonkero.png";
+import product4 from "./assets/morgan.png";
+import product5 from "./assets/sandels.png";
+import product6 from "./assets/smurffi.png";
+import product7 from "./assets/välivesi.png";
+import product8 from "./assets/vissy.png";
+
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "IPhone",
+    price: 999.0,
+    productImage: gambina,
+  },
+  {
+    id: 2,
+    productName: "Macbook Pro 2022 (M1)",
+    price: 1999.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Cannon M50 Camera",
+    price: 699.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "WLS Van Gogh Denim Jacket",
+    price: 228.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "LED Light Strips",
+    price: 19.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "SPECTRUM LS TEE",
+    price: 68.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "AUTO SERVICE SHIRT by GOLF WANG",
+    price: 120.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "DON'T TRIP UNSTRUCTURED HAT",
+    price: 40.0,
+    productImage: product8,
+  },
+];
