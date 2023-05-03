e9223a289dd23fe8202bdb08b71b04064dac01fd
Jorma <joel.oksanen1223@gmail.com>
2023-05-03 17:05:08 +0300

    1

--- src/pages/Alkoholittomat.js
+++ src/pages/Alkoholittomat.js
@@ -1,8 +1,25 @@
-{categories.map(category => (
+import React from 'react'
+
+
+
+
+
+export default function Alkoholittomat() {
+
+
+  {categories.map(category => (
     <li className="nav-item">
     <Link className="nav-link" to="/">{category.name}</Link>
   </li>
   ))}
+
+
+  
+  return (
+    <div>Tervetuloa Ryyst-verkkokkauppaan!</div>
+  )
+}
+
 
 
 
