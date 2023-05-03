5c93e7ddc6c2b598f25379cb65ca434006b9cc93
Jorma <joel.oksanen1223@gmail.com>
2023-05-03 17:15:51 +0300

    uusi kategoria

--- products/getproduct.php
+++ products/getproduct.php
@@ -2,9 +2,16 @@
 require_once '../inc/functions.php';
 require_once '../inc/headers.php';
 
-$uri = parse_url(filter_input(INPUT_SERVER,'PATH_INFO'),PHP_URL_PATH);
-$parameters = explode('/',$uri);
-$product_id = $parameters[1];
+// $uri = parse_url(filter_input(INPUT_SERVER,'PATH_INFO'),PHP_URL_PATH);
+//$parameters = explode('/',$uri);
+//$product_id = $parameters[1];
+
+
+$uri_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri_segments = explode('/', $uri_path);
+
+//print_r( $uri_segments); // for www.example.com/user/account you will get 'user'
+$product_id = $uri_segments[3];
 
 try {
   $db = openDb();
