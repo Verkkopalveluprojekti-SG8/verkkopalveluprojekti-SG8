aa65062367d1126bcf65c48509e800270a7f7513
vilmakilpelainen <n2kivi00@students.oamk.fi>
2023-05-03 19:24:31 +0300

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,15 @@ export default function Footer() {
   return (
     <footer className='footer'>
       <div className='container-fluid'>
-        <span>FOOTER
+        <span>
+          <h3>Jyyst Oy</h3>
+          <p>
+            Joel Oksanen <br></br>
+            Ville Rajala <br></br>
+            Vilma Kilpeläinen <br></br>
+            Joona Nurminen <br></br>
+            Mikael Karttunen
+          </p>
           </span>
       </div>
     </footer>
