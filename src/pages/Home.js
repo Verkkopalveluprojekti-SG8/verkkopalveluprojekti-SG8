e3210c7e5d274761ed55873fc56355aa9509347b
vilmakilpelainen <n2kivi00@students.oamk.fi>
2023-05-03 16:53:49 +0300

    kuva

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
     <div>Tervetuloa Ryyst-verkkokkauppaan!</div>
-    
+    <img src="/src/assets/alcohol-gb76d9e301_1920.png" alt='kuva'></img>
     </>
 
   )
